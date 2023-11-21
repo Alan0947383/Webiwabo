e90c89e1b51d4ef78914426204ebdf17ea63c543
Alan0947383 <134579220+Alan0947383@users.noreply.github.com>
2023-11-20 19:09:02 -0800

    Add files via upload

---
+++ Scriptss.js
@@ -0,0 +1,17 @@
+function Linkk(){
+  window.location.href="https://cuty.io/4JDH1";
+}
+
+
+function DISCORD(){
+  window.location.href="https://discord.gg/Qr6Ah9SAaw";
+}
+
+  
+  function YT(){
+  window.location.href="https://youtube.com/@Roblox12463cs?si=6UnaAhXCZdM70cSg";
+}
+
+function PAYPAL(){
+  window.location.href="https://Paypal.me/Www192922";
+}
